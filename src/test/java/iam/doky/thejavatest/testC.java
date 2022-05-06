5bbfa77888f5fc1adcec966e567a208c8401908b
freed <freed@freedui-MacBookPro.local>
2022-05-06 17:32:48 +0900

    test : for

---
+++ src/test/java/iam/doky/thejavatest/testC.java
@@ -0,0 +1,18 @@
+package iam.doky.thejavatest;
+
+import org.junit.jupiter.api.Test;
+
+public class testC {
+
+    public static void main(String[] args) {
+        int a = 0, i;
+
+        for (  i = 3; i < 100; i *= 3 ) {
+            a += i;
+        }
+
+        System.out.println(i);
+
+    }
+
+}
